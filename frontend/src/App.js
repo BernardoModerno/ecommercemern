c43bf08545b4955835dbf01d96e1a769699e2479
BernardoModerno <bernardo.moderno@gmail.com>
2023-08-19 23:56:46 -0300

    Admin and User Panel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,13 @@ import {
   Routes,
 } from 'react-router-dom';
 
+import AdminRoute from './components/Routes/AdminRoute';
 import PrivateRoute from './components/Routes/Private';
 import About from './pages/About';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import CreateCategory from './pages/Admin/CreateCategory';
+import CreateProduct from './pages/Admin/CreateProduct';
+import Users from './pages/Admin/Users';
 import ForgotPasssword from './pages/Auth/ForgotPasssword';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
@@ -13,6 +18,8 @@ import HomePage from './pages/HomePage';
 import Pagenotfound from './pages/Pagenotfound';
 import Policy from './pages/Policy';
 import Dashboard from './pages/user/Dashboard';
+import Orders from './pages/user/Orders';
+import Profile from './pages/user/Profile';
 
 function App() {
   return (
@@ -20,7 +27,15 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/users" element={<Users />} />
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPasssword />} />
